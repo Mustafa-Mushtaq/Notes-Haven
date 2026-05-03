0888882a69096f000efb5265abfa3f76872a946b
Mustafa-Mushtaq <mahir.mushtaq04@gmail.com>
2026-05-04 02:14:10 +0500

    server.js updated

--- server/server.js
+++ server/server.js
@@ -4,9 +4,11 @@ dotenv.config();
 import app from './src/app.js';
 import connectToDB from './src/config/db.js';
 
+const PORT = process.env.PORT || 3000;
+
 
 connectToDB();  
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
